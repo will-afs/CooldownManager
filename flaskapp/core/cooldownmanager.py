45fa28faacc0db2952a93230ee58d5a2709e8bf3
William AFONSO <william.afonso.pro@gmail.com>
2022-01-26 14:29:09 +0100

    Applied black and isort to reformat the code

--- flaskapp/core/cooldownmanager.py
+++ flaskapp/core/cooldownmanager.py
@@ -1,69 +1,77 @@
-import time
-from datetime import datetime, timedelta
-import time
-import toml
 import threading
+import time
+
+import toml
 from singleton_decorator import singleton
-import sys
 
 
 @singleton
-class CoolDownManager():
+class CoolDownManager:
     def __init__(self):
-        project_config = toml.load('project_config.toml')
+        project_config = toml.load("project_config.toml")
         self._cooldown_needed = False
-        self._cooldown_delay = project_config['time']['countdown_delay']
-        self._time_step = project_config['time']['time_step']
+        self._cooldown_delay = project_config["time"]["countdown_delay"]
+        self._time_step = project_config["time"]["time_step"]
 
     def _cooldown(self):
-        print('Cooling down...')
+        print("Cooling down...")
         time.sleep(self._cooldown_delay)
         self._cooldown_needed = False
-        print('Refreshed !')
+        print("Refreshed !")
 
     def get_token(self) -> bool:
-        if self._cooldown_needed == False:
+        if self._cooldown_needed is False:
             self._cooldown_needed = True
-            print('Generated token')
-            return True            
+            print("Generated token")
+            return True
         else:
-            while self._cooldown_needed == True:
-                print('Waiting for cooldown to generate token...')
-                while self._cooldown_needed == True:
+            while self._cooldown_needed is True:
+                print("Waiting for cooldown to generate token...")
+                while self._cooldown_needed is True:
                     time.sleep(self._time_step)
             return self.get_token()
-            
+
     def _run(self):
         while True:
-            if self._cooldown_needed == True:
-                print('Cooldown needed')
+            if self._cooldown_needed is True:
+                print("Cooldown needed")
                 self._cooldown()
             else:
                 time.sleep(self._time_step)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Usage example
     cooldownmanager = CoolDownManager()
-    print('\nInstanciated CoolDownManager\n')
+    print("\nInstanciated CoolDownManager\n")
 
     run_thread = threading.Thread(target=cooldownmanager._run, args=(), daemon=True)
     run_thread.start()
-    print('Started run_thread\n')
+    print("Started run_thread\n")
 
     # First token is returned directly : no need to cooldown at first
-    print('Asking for first token ...')
+    print("Asking for first token ...")
     request_time = time.time()
     cooldownmanager.get_token()
     response_time = time.time()
-    print('Obtained first token after {} s\n'.format(round(response_time-request_time, 2)))
+    print(
+        "Obtained first token after {} s\n".format(
+            round(response_time - request_time, 2)
+        )
+    )
 
     # Second token cannot be returned directly : need to respect cooldown delay
-    print('Asking for second token ...')
+    print("Asking for second token ...")
     request_time = time.time()
     cooldownmanager.get_token()
     response_time = time.time()
-    print('Obtained first token after {} s\n'.format(round(response_time-request_time, 2)))
+    print(
+        "Obtained first token after {} s\n".format(
+            round(response_time - request_time, 2)
+        )
+    )
 
-    print('Joining run thread')
-    run_thread.join(timeout=0) # Do not wait for run_thread to end before destroying it (infinite while loop)
-    print('End of the program')  
+    print("Joining run thread")
+    # Do not wait for run_thread to end (infinite while loop)
+    run_thread.join(timeout=0)
+    print("End of the program")
--- tests/test_cooldownmanager.py
+++ tests/test_cooldownmanager.py
@@ -1,29 +1,35 @@
+import threading
+import time
+import unittest
+
+import toml
+
 from flaskapp.core.cooldownmanager import CoolDownManager
 
-import time
-import threading
-import toml
-import unittest
 
 class TestCoolDownManager(unittest.TestCase):
     def setUp(self):
         self.cooldownmanager = CoolDownManager()
-        project_config = toml.load('project_config.toml')
-        self.ref_cooldown_delay = project_config['time']['countdown_delay']
-        self.tolerance = project_config['time']['tolerance']
+        project_config = toml.load("project_config.toml")
+        self.ref_cooldown_delay = project_config["time"]["countdown_delay"]
+        self.tolerance = project_config["time"]["tolerance"]
 
     def test_init(self):
         self.assertEqual(self.cooldownmanager._cooldown_delay, self.ref_cooldown_delay)
-    
+
     def test_cooldown(self):
         time_0 = time.time()
         self.cooldownmanager._cooldown()
         time_1 = time.time()
-        self.assertGreaterEqual(time_1-time_0, self.ref_cooldown_delay)
-        self.assertAlmostEqual(time_1-time_0, self.ref_cooldown_delay, delta=self.tolerance)
+        self.assertGreaterEqual(time_1 - time_0, self.ref_cooldown_delay)
+        self.assertAlmostEqual(
+            time_1 - time_0, self.ref_cooldown_delay, delta=self.tolerance
+        )
 
     def test_get_token(self):
-        run_thread = threading.Thread(target=self.cooldownmanager._run, args=(), daemon=True)
+        run_thread = threading.Thread(
+            target=self.cooldownmanager._run, args=(), daemon=True
+        )
         run_thread.start()
 
         time_0 = time.time()
@@ -32,16 +38,22 @@ class TestCoolDownManager(unittest.TestCase):
 
         # no cooldown needed : token should be returned directly
         self.assertEqual(token_1, True)
-        self.assertAlmostEqual(float(time_1-time_0), float(0), delta=float(self.tolerance))
+        self.assertAlmostEqual(
+            float(time_1 - time_0), float(0), delta=float(self.tolerance)
+        )
 
         # cooldown needed : token should only be returned after cooldown delay
         time_2 = time.time()
         token_2 = self.cooldownmanager.get_token()
         time_3 = time.time()
-        run_thread.join(timeout=0) # Do not wait for run_thread to end before destroying it (infinite while loop)
-        self.assertLessEqual(time_2-time_1, self.ref_cooldown_delay) # Otherwise this test would be pointless
+        run_thread.join(
+            timeout=0
+        )  # Do not wait for run_thread to end before destroying it (infinite while loop)
+        self.assertLessEqual(
+            time_2 - time_1, self.ref_cooldown_delay
+        )  # Otherwise this test would be pointless
         self.assertEqual(token_2, True)
-        self.assertGreaterEqual(time_3-time_1, self.ref_cooldown_delay)
-        self.assertAlmostEqual(time_3-time_1, self.ref_cooldown_delay, delta=self.tolerance)
-
-        
+        self.assertGreaterEqual(time_3 - time_1, self.ref_cooldown_delay)
+        self.assertAlmostEqual(
+            time_3 - time_1, self.ref_cooldown_delay, delta=self.tolerance
+        )
